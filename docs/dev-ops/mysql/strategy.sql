0d387ad04a738d1129cb4202199f1a31c501f2e5
dasi <1740929297@qq.com>
2025-11-30 19:23:23 +0800

    【第21节】 1. 充值仍有部分问题 2. 增加了 awardQuery，专门处理对API对奖品的查询

--- docs/dev-ops/mysql/strategy.sql
+++ docs/dev-ops/mysql/strategy.sql
@@ -11,9 +11,11 @@ CREATE TABLE award (
     update_time  DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT ='奖品表';
 INSERT INTO award (award_id, award_name, award_config, award_desc)
-VALUES (2001, '测试奖品2001-随机积分', '1,100', '测试描述2001'),
-       (2002, '测试奖品2002-2元优惠券', NULL, '测试描述2002'),
-       (2003, '测试奖品2003-IPhone17', NULL, '测试描述2003');
+VALUES (2000, '【测试奖品2000】随机积分', '1,100', '负责兜底的随机积分'),
+       (2001, '【测试奖品2001】随机积分', '1,50', '可以抽到的随机积分'),
+       (2002, '【测试奖品2002】5元优惠券', NULL, '普通奖品'),
+       (2003, '【测试奖品2003】IPhone17', NULL, '稀有奖品'),
+       (2004, '【测试奖品2004】1积分', NULL, '黑名单奖品');
 
 DROP TABLE IF EXISTS strategy;
 CREATE TABLE strategy (
@@ -25,7 +27,7 @@ CREATE TABLE strategy (
     update_time   DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT ='策略表';
 INSERT INTO strategy (strategy_id, strategy_desc, rule_models)
-VALUES (1001, '测试策略1001', 'rule_blacklist,rule_weight');
+VALUES (1001, '【测试策略1001】黑名单+权重', 'rule_blacklist,rule_weight');
 
 DROP TABLE IF EXISTS strategy_rule;
 CREATE TABLE strategy_rule (
@@ -39,27 +41,28 @@ CREATE TABLE strategy_rule (
     UNIQUE KEY uq_strategy_id_rule_model (strategy_id, rule_model)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT ='策略规则表';
 INSERT INTO strategy_rule (strategy_id, rule_model, rule_value, rule_desc)
-VALUES (1001, 'rule_blacklist', '100:dasi', '黑名单：dasi'),
+VALUES (1001, 'rule_blacklist', '2004:dasi', '黑名单：dasi'),
        (1001, 'rule_weight', '4000:2001,2002 5000:2002,2003', '权重：4000 和 5000');
 
 DROP TABLE IF EXISTS strategy_award;
 CREATE TABLE strategy_award (
-    id            BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '自增id',
-    strategy_id   BIGINT         NOT NULL COMMENT '抽奖策略id',
-    award_id      INT            NOT NULL COMMENT '抽奖奖品id',
-    tree_id       VARCHAR(32)    NULL COMMENT '规则树id',
-    award_title   VARCHAR(32)    NOT NULL COMMENT '抽奖奖品标题',
-    award_total   INT            NOT NULL DEFAULT 0 COMMENT '奖品库存总量',
-    award_surplus INT            NOT NULL DEFAULT 0 COMMENT '奖品库存余量',
-    award_rate    DECIMAL(10, 6) NOT NULL DEFAULT 0 COMMENT '奖品中奖概率',
-    create_time   DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    update_time   DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    id             BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '自增id',
+    strategy_id    BIGINT         NOT NULL COMMENT '抽奖策略id',
+    award_id       INT            NOT NULL COMMENT '抽奖奖品id',
+    tree_id        VARCHAR(32)    NULL COMMENT '规则树id',
+    award_title    VARCHAR(256)   NULL COMMENT '抽奖奖品标题',
+    award_allocate INT            NOT NULL COMMENT '奖品库存总量',
+    award_surplus  INT            NOT NULL COMMENT '奖品库存余量',
+    award_rate     DECIMAL(10, 6) NOT NULL COMMENT '奖品中奖概率',
+    award_index    INT            NOT NULL COMMENT '奖品排列序号',
+    create_time    DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    update_time    DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     UNIQUE KEY uq_strategy_id_award_id (strategy_id, award_id)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT ='策略奖品表';
-INSERT INTO strategy_award (strategy_id, award_id, tree_id, award_title, award_total, award_surplus, award_rate)
-VALUES (1001, 2001, 'tree_test','测试：策略1001奖品2001', 15, 15, 0.1500),
-       (1001, 2002, 'tree_test','测试：策略1001奖品2002', 80, 80, 0.8000),
-       (1001, 2003, 'tree_test','测试：策略1001奖品2003', 5, 5, 0.0500);
+INSERT INTO strategy_award (strategy_id, award_id, tree_id, award_title, award_allocate, award_surplus, award_rate, award_index)
+VALUES (1001, 2001, 'tree_stock','【测试策略1001测试奖品2001】普通奖品，走tree_stock', 15, 15, 0.1500, 1),
+       (1001, 2002, 'tree_stock','【测试策略1001测试奖品2002】普通奖品，走tree_stock', 80, 80, 0.8000, 2),
+       (1001, 2003, 'tree_lock','【测试策略1001测试奖品2003】稀有奖品，走tree_stock', 5, 5, 0.0500, 3);
 
 DROP TABLE IF EXISTS rule_tree;
 CREATE TABLE rule_tree (
@@ -72,7 +75,8 @@ CREATE TABLE rule_tree (
     update_time DATETIME           NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT ='规则树表';
 INSERT INTO rule_tree (tree_id, tree_name, tree_desc, tree_root)
-VALUES ('tree_test', '测试规则树tree_test', '测试规则树描述tree_test', 'rule_lock');
+VALUES ('tree_lock', '测试规则树tree_lock', '先走 lock 再走 stock', 'rule_lock'),
+       ('tree_stock', '测试规则树tree_stock', '直接走 stock', 'rule_stock');
 
 DROP TABLE IF EXISTS rule_node;
 CREATE TABLE rule_node (
@@ -82,12 +86,15 @@ CREATE TABLE rule_node (
     rule_model  VARCHAR(32)     NOT NULL COMMENT '规则树节点模型',
     rule_value  VARCHAR(512)    NULL DEFAULT NULL COMMENT '规则树节点值',
     create_time DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    update_time DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
+    update_time DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    UNIQUE KEY uq_tree_id_rule_model (tree_id, rule_model)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT ='规则树节点表';
 INSERT INTO rule_node (tree_id, rule_model, rule_desc, rule_value)
-VALUES ('tree_test', 'rule_lock', '达到一定次数解锁', '3'),
-       ('tree_test', 'rule_luck', '幸运奖品', '2001'),
-       ('tree_test', 'rule_stock', '库存扣减', NULL);
+VALUES ('tree_lock', 'rule_lock', '【rule_lock】达到3次才解锁', '3'),
+       ('tree_lock', 'rule_stock', '【rule_stock】库存扣减1', NULL),
+       ('tree_lock', 'rule_luck', '【rule_luck】幸运奖品2000', '2000'),
+       ('tree_stock', 'rule_stock', '【rule_stock】库存扣减1', NULL),
+       ('tree_stock', 'rule_luck', '【rule_luck】幸运奖品2000', '2000');
 
 DROP TABLE IF EXISTS rule_edge;
 CREATE TABLE rule_edge (
@@ -98,9 +105,11 @@ CREATE TABLE rule_edge (
     rule_check_type   VARCHAR(32)     NOT NULL COMMENT '规则检查类型',
     rule_check_result VARCHAR(32)     NOT NULL COMMENT '规则检查结果',
     create_time       DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    update_time       DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
+    update_time       DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    UNIQUE KEY uq_tree_id_from_to (tree_id, rule_node_from, rule_node_to)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT ='规则树边表';
 INSERT INTO rule_edge (tree_id, rule_node_from, rule_node_to, rule_check_type, rule_check_result)
-VALUES ('tree_test', 'rule_lock', 'rule_stock', 'EQUAL', 'PERMIT'),
-       ('tree_test', 'rule_lock', 'rule_luck', 'EQUAL', 'CAPTURE'),
-       ('tree_test', 'rule_stock', 'rule_luck', 'EQUAL', 'PERMIT');
+VALUES ('tree_lock', 'rule_lock', 'rule_stock', 'EQUAL', 'PERMIT'),
+       ('tree_lock', 'rule_lock', 'rule_luck', 'EQUAL', 'CAPTURE'),
+       ('tree_lock', 'rule_stock', 'rule_luck', 'EQUAL', 'CAPTURE'),
+       ('tree_stock', 'rule_stock', 'rule_luck', 'EQUAL', 'CAPTURE');
